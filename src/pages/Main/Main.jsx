6b2f6f47c8342407b44a1567730fc70add300768
Dmitry Naumov <komrad.nda@yandex.ru>
2025-06-27 16:12:57 +0300

    add categories

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,6 @@
-import {useEffect, useState} from 'react'
-import {getNews} from '../../api/apiNews'
+import { useEffect, useState } from 'react'
+import { getCategories, getNews } from '../../api/apiNews'
+import Categories from '../../components/Categories/Categories'
 import NewsBanner from '../../components/NewsBanner/NewsBanner'
 import NewsList from '../../components/NewsList/NewsList'
 import Pagination from '../../components/Pagination/Pagination'
@@ -10,6 +11,8 @@ const Main = () => {
 	const [news, setNews] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
 	const [currentPage, setCurrentPage] = useState(1)
+	const [categories, setCategories] = useState([])
+	const [selectedCategory, setSelectedCategory] = useState('All')
 
 	const totalPages = 10
 	const pageSize = 10
@@ -17,7 +20,11 @@ const Main = () => {
 	const fetchNews = async currentPage => {
 		try {
 			setIsLoading(true)
-			const response = await getNews(currentPage, pageSize)
+			const response = await getNews({
+				page_number: currentPage,
+				page_size: pageSize,
+				category: selectedCategory === 'All' ? null : selectedCategory,
+			})
 			setNews(response.news)
 			setIsLoading(false)
 		} catch (error) {
@@ -25,9 +32,22 @@ const Main = () => {
 		}
 	}
 
+	const fetchCategories = async () => {
+		try {
+			const response = await getCategories()
+			setCategories(['All', ...response.categories])
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchCategories()
+	}, [])
+
 	useEffect(() => {
 		fetchNews(currentPage)
-	}, [currentPage])
+	}, [currentPage, selectedCategory])
 
 	const handleNextPage = () => {
 		if (currentPage < totalPages) {
@@ -47,10 +67,8 @@ const Main = () => {
 
 	return (
 		<main className={styles.main}>
-			{news.length > 0 && !isLoading ? <NewsBanner item={news[0]} /> : <Skeleton
-				count={1}
-				type="banner"
-			/>}
+			<Categories categories={categories} setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory} />
+			{news.length > 0 && !isLoading ? <NewsBanner item={news[0]} /> : <Skeleton count={1} type='banner' />}
 			<Pagination
 				totalPages={totalPages}
 				handleNextPage={handleNextPage}
@@ -58,10 +76,7 @@ const Main = () => {
 				handlePageClick={handlePageClick}
 				currentPage={currentPage}
 			/>
-			{!isLoading ? <NewsList news={news} /> : <Skeleton
-				count={10}
-				type={'item'}
-			/>}
+			{!isLoading ? <NewsList news={news} /> : <Skeleton count={10} type={'item'} />}
 			<Pagination
 				totalPages={totalPages}
 				handleNextPage={handleNextPage}
